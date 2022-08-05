eee2b60351241a83039f847b033b0d5c6332ced1
stndb01 <54304759+stndb01@users.noreply.github.com>
2022-08-05 22:26:18 +0700

    Update v_programKerja.php

--- v_programKerja.php
+++ v_programKerja.php
@@ -55,7 +55,7 @@
                             $p['namaProgram']; ?></td>
                         <td><?php echo
                             $p['suratKeterangan']; ?></td>
-                        <td> <a href=index.php?edit=$row['nomorProgram']>Edit</a></td>
+                        <td> <a href=index.php?edit&value=<?php echo $p['nomorProgram'];?> >Edit</a></td>
                         <td> <a href=index.php?delete=$row['nomorProgram']>Delete</a></td>
                     </tr>
                 <?php }; ?>
